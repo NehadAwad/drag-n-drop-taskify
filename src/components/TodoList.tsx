36f6983cdb7f7f917eeac24fcad0ced4b34e556d
nehadawad <nehadakash622@gmail.com>
2023-05-12 00:53:48 +0600

    feat: crud completed

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Todo } from "../model";
 import './styles.css';
+import { SingleTodo } from "./SingleTodo";
 
 interface Props {
     todos: Todo[];
@@ -9,10 +10,17 @@ interface Props {
 
 export const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
 
-    return(
+    return (
         <div className="todos">
             {todos.map((todo) => (
-                <li key={todo.id}>{todo.todo}</li>
+                // <li key={todo.id}>{todo.todo}</li>
+                <SingleTodo
+                    todo={todo} 
+                    key={todo.id}
+                    todos={todos}
+                    setTodos={setTodos} 
+                />
+
             ))}
         </div>
     )
