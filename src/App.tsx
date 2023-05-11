c1b66b97a3e77367943cedd1be7aee4fee5283a0
nehadawad <nehadakash622@gmail.com>
2023-05-11 13:10:26 +0600

    todo list component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
 import { useState } from 'react'
 import './App.css'
 import { InputFeild } from './components/InputFeild'
+import { Todo } from './model';
+import { TodoList } from './components/TodoList';
 
 const App: React.FC = () => {
 
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if(todo) {
+      setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}]);
+      setTodo("");
+    }
+    console.log("trigger")
+  };
+
   
+  console.log(todos)
   return (
     <>
       <div className="App">
         <span className="heading">Taskify</span>
-        <InputFeild todo={todo} setTodo={setTodo}/> 
+        <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+        <TodoList todos={todos} setTodos={setTodos}/>
       </div>
-      
     </>
   )
 }
--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './styles.css';
 
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-export const InputFeild = ({todo, setTodo}: Props)  => {
+export const InputFeild: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   return (
-   <form action="" className='input'>
-    <input type="input" placeholder='Enter a task' className='input__box' />
-    <button className='input__submit' type='submit'>Go</button>
-   </form>
+    <form action="" className='input' onSubmit={(e) => {
+      handleAdd(e)
+      inputRef.current?.blur();
+    }}>
+      <input
+        ref={inputRef}
+        type="input"
+        placeholder='Enter a task'
+        className='input__box'
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button className='input__submit' type='submit'>Go</button>
+    </form>
   )
 }
- 
